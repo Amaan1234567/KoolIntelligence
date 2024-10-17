bb8400b561758cad331d0312a269b641bc1617a3
Rahul Vadhyar <107788610+RahulVadhyar@users.noreply.github.com>
2024-10-17 21:22:13 +0530

    terminal integration +  make UI better (#26)
    * Added resizable panel
    
    * make chat be responsive
    
    * Add libvterm as a dependency
    
    * remove vterm as a dependency
    
    * terminal integration + sp[lit into files
    
    * update docs
    
    * add logs
    
    * apply clang format
    
    * disable clang format for terminal/lib
    
    ---------
    
    Co-authored-by: shrenisc <shreya.shastry@gmail.com>

---
+++ src/ui/terminal/lib/KonsoleQML.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2023 Jonah Brüchert <jbb@kaidan.im>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef QMLTERMWIDGET_PLUGIN_H
+#define QMLTERMWIDGET_PLUGIN_H
+
+struct KonsoleQML {
+    static void registerTypes(const char *uri);
+};
+
+#endif // QMLTERMWIDGET_PLUGIN_H
