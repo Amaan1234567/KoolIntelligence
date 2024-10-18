263aa898fa569b8fac672e932014b20b5cf3be96
= <=>
2024-10-18 17:10:34 +0530

    made the changes requested except one

--- src/model_backend/tts_service.cpp
+++ src/model_backend/tts_service.cpp
@@ -1,5 +1,12 @@
 #include "tts_service.hpp"
 
+TTSService::TTSService()
+{
+    if (!std::filesystem::exists(std::string(getenv("HOME")) + "/koolintelligence/models/ggml_weights_q4.bin")) {
+        system("chmod +x src/model_backend/download_voice_out_model.sh && src/model_backend/download_voice_out_model.sh");
+    }
+}
+
 void TTSService::speak(std::string text)
 {
     system(("voice_out -p " + std::string("\"" + text + "\"") + " -m " + std::string(getenv("HOME")) + "/koolintelligence/models/ggml_weights_q4.bin -o "
--- src/model_backend/tts_service.hpp
+++ src/model_backend/tts_service.hpp
@@ -4,12 +4,7 @@
 class TTSService
 {
 public:
-    TTSService()
-    {
-        if (!std::filesystem::exists(std::string(getenv("HOME")) + "/koolintelligence/models/ggml_weights_q4.bin")) {
-            system("chmod +x src/model_backend/download_voice_out_model.sh && src/model_backend/download_voice_out_model.sh");
-        }
-    }
+    TTSService();
 
     void speak(std::string text);
 };
